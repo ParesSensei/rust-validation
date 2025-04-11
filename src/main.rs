71628e19e9b5f00b28fa8e564c70bf87e65fb5af
Pares <alfariz2004@gmail.com>
2025-04-11 20:00:18 +0700

    validation nested struct

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,63 @@ struct LoginRequest {
     password: String,
 }
 
+#[derive(Debug, Validate)]
+struct AddressRequest {
+    #[validate(length(min = 1, max = 100))]
+    street: String,
+    #[validate(length(min = 1, max = 100))]
+    city: String,
+    #[validate(length(min = 1, max = 100))]
+    country: String,
+}
+
+#[derive(Debug, Validate)]
+struct RegisterUserRequest {
+    #[validate(length(min = 3, max = 20))]
+    username: String,
+    #[validate(length(min = 3, max = 20))]
+    password: String,
+    #[validate(length(min = 3, max = 100))]
+    name: String,
+    #[validate(nested)]
+    address: AddressRequest,
+}
+
+#[test]
+fn test_nested_struct_success() {
+    let request = RegisterUserRequest {
+        username: "ekoatro".to_string(),
+        password: "passwortaro".to_string(),
+        name: "ekotaro".to_string(),
+        address: AddressRequest{
+            street: "jalan".to_string(),
+            city: "kota".to_string(),
+            country: "negara japantaro".to_string(),
+        }
+    };
+
+    assert!(request.validate().is_ok());
+}
+
+#[test]
+fn test_nested_struct_error() {
+    let request = RegisterUserRequest {
+        username: "ekoatro".to_string(),
+        password: "passwortaro".to_string(),
+        name: "ekotaro".to_string(),
+        address: AddressRequest{
+            street: "".to_string(),
+            city: "".to_string(),
+            country: "".to_string(),
+        }
+    };
+
+    assert!(request.validate().is_err());
+
+    let errors: ValidationErrors = request.validate().err().unwrap();
+    println!("{:#?}", errors.errors());
+}
+
 #[test]
 fn test_validate_success() {
     let login = LoginRequest {
