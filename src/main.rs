c9ba7fb078bc3709f1c0e9a952a03697a060736c
Pares <alfariz2004@gmail.com>
2025-04-11 22:12:04 +0700

    custom validation, struct level validation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 use serde::Serialize;
 use validator::{Validate, ValidationErrors};
 
+pub mod pzn {
+    pub mod validator {
+        use crate::RegisterUserRequest;
+        use std::borrow::Cow;
+        use validator::ValidationError;
+
+        pub fn not_blank(value: &str) -> Result<(), ValidationError> {
+            if value.trim().is_empty() {
+                return Err(ValidationError::new("not_blank")
+                    .with_message(Cow::from("Value cannot be blank")));
+            }
+
+            Ok(())
+        }
+
+        pub fn password_equals_confirm_password(
+            request: &RegisterUserRequest,
+        ) -> Result<(), ValidationError> {
+            if request.password != request.confirm_password {
+                return Err(ValidationError::new("password_equals_confirm_password")
+                    .with_message(Cow::from("Password and confirm password must be same")));
+            }
+
+            Ok(())
+        }
+    }
+}
+
+#[derive(Debug, Validate, Serialize)]
+struct CreateCategoryRequest {
+    #[validate(custom(function = "crate::pzn::validator::not_blank"))]
+    id: String,
+    #[validate(custom(function = "crate::pzn::validator::not_blank"))]
+    name: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -32,11 +68,17 @@ struct AddressRequest {
 }
 
 #[derive(Debug, Validate)]
-struct RegisterUserRequest {
+#[validate(schema(
+    function = "pzn::validator::password_equals_confirm_password",
+    skip_on_field_errors = false
+))]
+pub struct RegisterUserRequest {
     #[validate(length(min = 3, max = 20))]
     username: String,
     #[validate(length(min = 3, max = 20))]
     password: String,
+    #[validate(length(min = 3, max = 20))]
+    confirm_password: String,
     #[validate(length(min = 3, max = 100))]
     name: String,
     #[validate(nested)]
@@ -49,7 +91,7 @@ struct Product {
     id: String,
     #[validate(length(min = 3, max = 200))]
     name: String,
-    #[validate(nested, length(min =1))]
+    #[validate(nested, length(min = 1))]
     variants: Vec<ProductVariant>,
 }
 
@@ -62,20 +104,31 @@ struct ProductVariant {
 }
 
 #[test]
+fn test_custom_validation() {
+    let request = CreateCategoryRequest {
+        id: "".to_string(),
+        name: "        ".to_string(),
+    };
+
+    let errors: ValidationErrors = request.validate().err().unwrap();
+    println!("{:?}", errors.errors());
+}
+
+#[test]
 fn test_validate_vector_success() {
-    let request = Product{
+    let request = Product {
         id: "product-1".to_string(),
         name: "product-1".to_string(),
         variants: vec![
-            ProductVariant{
+            ProductVariant {
                 name: "variant-1".to_string(),
-                price: 1000
+                price: 1000,
             },
-            ProductVariant{
+            ProductVariant {
                 name: "variant-2".to_string(),
-                price: 2000
+                price: 2000,
             },
-        ]
+        ],
     };
 
     assert!(request.validate().is_ok());
@@ -83,19 +136,19 @@ fn test_validate_vector_success() {
 
 #[test]
 fn test_validate_vector_error() {
-    let request = Product{
+    let request = Product {
         id: "product-1".to_string(),
         name: "product-1".to_string(),
         variants: vec![
-            ProductVariant{
+            ProductVariant {
                 name: "".to_string(),
-                price: -1000
+                price: -1000,
             },
-            ProductVariant{
+            ProductVariant {
                 name: "".to_string(),
-                price: -2000
+                price: -2000,
             },
-        ]
+        ],
     };
 
     assert!(request.validate().is_err());
@@ -108,12 +161,13 @@ fn test_nested_struct_success() {
     let request = RegisterUserRequest {
         username: "ekoatro".to_string(),
         password: "passwortaro".to_string(),
+        confirm_password: "passwortaro".to_string(),
         name: "ekotaro".to_string(),
-        address: AddressRequest{
+        address: AddressRequest {
             street: "jalan".to_string(),
             city: "kota".to_string(),
             country: "negara japantaro".to_string(),
-        }
+        },
     };
 
     assert!(request.validate().is_ok());
@@ -124,12 +178,13 @@ fn test_nested_struct_error() {
     let request = RegisterUserRequest {
         username: "ekoatro".to_string(),
         password: "passwortaro".to_string(),
+        confirm_password: "salah".to_string(),
         name: "ekotaro".to_string(),
-        address: AddressRequest{
+        address: AddressRequest {
             street: "".to_string(),
             city: "".to_string(),
             country: "".to_string(),
-        }
+        },
     };
 
     assert!(request.validate().is_err());
@@ -158,5 +213,5 @@ fn test_validate_failed() {
     assert!(login.validate().is_err());
 
     let errors: ValidationErrors = login.validate().err().unwrap();
-    println!("errors: {:?}", errors);
+    println!("errors: {:#?}", errors);
 }
