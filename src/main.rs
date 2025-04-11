3d2dd1c7d8402981de0822198b31ece94b7b6253
Pares <alfariz2004@gmail.com>
2025-04-11 10:26:04 +0700

    struct validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use validator::{Validate, ValidationErrors};
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Validate)]
+struct LoginRequest {
+    #[validate(length(
+        min = 3,
+        max = 20,
+        message = "username length must be between 3 and 20"
+    ))]
+    username: String,
+    #[validate(length(
+        min = 3,
+        max = 20,
+        message = "password length must be between 3 and 20"
+    ))]
+    password: String,
+}
+
+#[test]
+fn test_validate_success() {
+    let login = LoginRequest {
+        username: "eko".to_string(),
+        password: "rahasia".to_string(),
+    };
+
+    assert!(login.validate().is_ok());
+}
+
+#[test]
+fn test_validate_failed() {
+    let login = LoginRequest {
+        username: "ek".to_string(),
+        password: "rahasia".to_string(),
+    };
+
+    assert!(login.validate().is_err());
+
+    let errors: ValidationErrors = login.validate().err().unwrap();
+    println!("errors: {:?}", errors);
 }
